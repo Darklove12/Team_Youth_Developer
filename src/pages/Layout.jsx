25ac13236025e726af8090b18c8c719ac92f3b80
Darklove12 <bsinukela31@gmail.com>
2023-07-05 11:33:59 +0200

    div elements

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,7 +3,12 @@ import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-  
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
 };
 
 export default Layout;

293a23586e81d893d97040e67472716443bf8ef0
Darklove12 <bsinukela31@gmail.com>
2023-07-05 13:44:37 +0200

    made import react

--- src/pages/Viewinfro.jsx
+++ src/pages/Viewinfro.jsx
@@ -1,28 +1,3 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
-import Viewdata from '../pages/Viewdata';
-
-function Viewinfro(){
-
-    const {name} = useParams();
-
-  return (
-    <div className="container">
-        {
-            Viewdata.filter((card )=> card.name === name).map ((card,index)=>(
-                <div key={index} >
-
-                <img src={card.image} alt="" />
-                <h2>{card.name}</h2>
-                <h4>{card.price}</h4>
-
-                </div>
-            )
-
-            )
-        }
-    </div>
-  )
-}
 
 export default Viewinfro;

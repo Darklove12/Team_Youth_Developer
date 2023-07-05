58a74254c1b5565e9a1cf6fd27ab5d46e512f17e
Darklove12 <bsinukela31@gmail.com>
2023-07-05 12:47:47 +0200

    made id, name and image

--- src/pages/ProductData.js
+++ src/pages/ProductData.js
@@ -1,2 +1,29 @@
-const ProductData=[]
+const ProductData=[
+        {
+            id: 1, name: 'MAXI DRESS',Image:'https://shacara.com/members/3664/uploads/63f77e8bdacd82.92939689.jpg'
+        },
+        {
+            id: 2, name: 'Allyn Fashion',Image:'https://shacara.com/members/40/uploads/5fe86a67200547.72611963.jpg'
+        },
+        {
+            id: 3, name: 'SIMA BREW',Image:'https://shacara.com/members/5/uploads/5d3b64ba7b54b4.64319625.jpg'
+        },
+        {
+            id: 4, name: 'MOCK NECK ',Image:'https://shacara.com/members/3664/uploads/63ca979241c227.67026428.jpg'
+        },
+    
+        {
+            id: 5, name: 'SKINNY JACKET',Image:'https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/3233222/original.jpg'
+        },
+        {
+            id: 6, name: 'SOHO SLIM FIT',Image:'https://assets.superbalistcdn.co.za/300x432/filters:quality(75):format(jpg)/3036124/original.jpg'
+        },
+        {
+            id: 7, name: 'HI-TIE',Image:'https://m.media-amazon.com/images/I/81qCnXvFEvL._AC_UL600_FMwebp_QL65_.jpg'
+        },
+        {
+            id: 8, name: 'MEN FASHION BAG',Image:'https://images.unsplash.com/photo-1490427712608-588e68359dbd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8TWVuJTIwZmFzaGlvbiUyMGJhZ3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60'
+        }
+    
+]
 export default ProductData

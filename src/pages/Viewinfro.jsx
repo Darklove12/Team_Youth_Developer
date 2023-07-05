9a3ef537ce26d285468cc3e640a864ca9af02357
Darklove12 <bsinukela31@gmail.com>
2023-07-05 13:53:17 +0200

    added object

--- src/pages/Viewinfro.jsx
+++ src/pages/Viewinfro.jsx
@@ -8,7 +8,19 @@ function Viewinfro(){
 
   return (
     <div className="container">
-      
+        {
+            Viewdata.filter((card )=> card.name === name).map ((card,index)=>(
+                <div key={index} >
+  
+                <img src={card.image} alt="" />
+                <h2>{card.name}</h2>
+                <h4>{card.price}</h4>
+  
+                </div>
+            )
+  
+            )
+        }
     </div>
   )
 }

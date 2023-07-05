957fc70b36ea3050c3ce03a236cd6f42bdd5e5d2
Darklove12 <bsinukela31@gmail.com>
2023-07-05 09:19:06 +0200

    added import React and useState. export default

--- src/pages/CategoryProducts.jsx
+++ src/pages/CategoryProducts.jsx
@@ -1,29 +1,3 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import {data} from "../data";
-import Product from '../components/Product'
-
-const CategoryProducts = () => {
-  const { name } = useParams();
-
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    let p = data.filter((product) => product.category === name);
-
-    setProducts(p);
-  }, []);
-
-  console.log(products);
-
-  return (
-    <div className="categoryPoducts">
-      <div className="header">Category{name}</div>
-      <div className="products">
-        {products?.map((product) => <Product key={product.id} product={product} />)}
-      </div>
-    </div>
-  );
-};
 
 export default CategoryProducts;

4e3e133d26d136341b4d5f21a6f0eb9643277d3d
Darklove12 <bsinukela31@gmail.com>
2023-07-05 09:10:35 +0200

    added import Productcard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Slider from "../components/Slider";
 import Category from "../components/Category";
+import Productcard from "../components/Productcard";
 
 
 export default Home;

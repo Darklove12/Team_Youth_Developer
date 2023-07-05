bc6b89313ec93ee5d72c4d69bee7e94db73bdd28
Darklove12 <bsinukela31@gmail.com>
2023-07-05 09:41:46 +0200

    added import Product

--- src/pages/CategoryProducts.jsx
+++ src/pages/CategoryProducts.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {data} from "../data";
+import Product from '../components/Product'
+
+
 
 export default CategoryProducts;

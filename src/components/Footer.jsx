aa048b8ac095b6395e99efd54014289a417a5003
Darklove12 <bsinukela31@gmail.com>
2023-07-11 13:47:24 +0200

    fixed all lis

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,16 @@ function footer() {
     <div class="container">
     <div class="row">
     <div class="col-sm-12 col-md-6">
-            
+    <h6>Neighborhood Market Info</h6>
+            <div class="col-xs-6 col-md-3">
+            <ul class="footer-links">
+              <li><a href="/home">About us</a></li>
+              <li><a href="/NewArrivals">Careers</a></li>
+              <li><a href="/Men">Find Store</a></li>
+              <li><a href="Ladies">Term of Serivice</a></li>
+              <li><a href="/Promos">Refund Policy</a></li>
+            </ul>
+         
           </div>
         </div>
       </div>

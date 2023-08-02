9228abdf0f023e0b2a59fd5172893dd44c248f82
unknown <you@example.com>
2023-08-02 09:39:01 +0200

    change all

--- src/components/Promos.jsx
+++ src/components/Promos.jsx
@@ -27,7 +27,7 @@ function Promos() {
                                     <MDBCol lg="7">
                                         <MDBTypography tag="h5">
                                             <a href="#!" className="text-body">
-                                                <MDBIcon fas icon="long-arrow-alt-left me-2" /> Continue
+                                                <MDBIcon fas icon="long-arrow-alt-left me-2" /> Promos
                                                 shopping
                                             </a>
                                         </MDBTypography>
@@ -37,7 +37,7 @@ function Promos() {
                                         <div className="d-flex justify-content-between align-items-center mb-4">
                                             <div>
                                                 <p className="mb-1">Shopping cart</p>
-                                                <p className="mb-0">You have 4 items in your cart</p>
+                                                <p className="mb-0">You have 12 items in your cart</p>
                                             </div>
                                             <div>
                                                 <p>
@@ -56,15 +56,15 @@ function Promos() {
                                                     <div className="d-flex flex-row align-items-center">
                                                         <div>
                                                             <MDBCardImage
-                                                                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
+                                                                src="https://ae01.alicdn.com/kf/Sf195866fb29340778bc6b13b51c952a6i/Tracksuits-Women-Casual-Solid-Warm-Suits-Hoodies-Sweatpants-Autumn-Winter-Pullover-Sweatshirts-Pants-Sports-Suit-Two.jpg_220x220q90.jpg_.webp"
                                                                 fluid className="rounded-3" style={{ width: "65px" }}
                                                                 alt="Shopping item" />
                                                         </div>
                                                         <div className="ms-3">
-                                                            <MDBTypography tag="h5">
-                                                                Iphone 11 pro
+                                                            <MDBTypography tag="h5">  
                                                             </MDBTypography>
-                                                            <p className="small mb-0">256GB, Navy Blue</p>
+                                                            <p className="small mb-0">Ladies Sweaters and Joggers</p>
+                                                            <p className="small mb-0">yellow, blue , black</p>
                                                         </div>
                                                     </div>
                                                     <div className="d-flex flex-row align-items-center">
@@ -75,7 +75,7 @@ function Promos() {
                                                         </div>
                                                         <div style={{ width: "80px" }}>
                                                             <MDBTypography tag="h5" className="mb-0">
-                                                                R19 000
+                                                                R500
                                                             </MDBTypography>
                                                         </div>
                                                         <a href="#!" style={{ color: "#cecece" }}>
@@ -92,15 +92,15 @@ function Promos() {
                                                     <div className="d-flex flex-row align-items-center">
                                                         <div>
                                                             <MDBCardImage
-                                                                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img2.webp"
+                                                                src="https://media.istockphoto.com/id/1085149318/photo/sale-sign-at-the-entrance-of-clothing-store.webp?b=1&s=170667a&w=0&k=20&c=ulyPJOrWKbpuOZjSjmROQjDBpTHGzAvt1C5m3dyL_rA="
                                                                 fluid className="rounded-3" style={{ width: "65px" }}
                                                                 alt="Shopping item" />
                                                         </div>
                                                         <div className="ms-3">
                                                             <MDBTypography tag="h5">
-                                                                Samsung galaxy Note 10
+                                                            ladies Knitwear
                                                             </MDBTypography>
-                                                            <p className="small mb-0">256GB, Navy Blue</p>
+                                                            <p className="small mb-0">white , red , black</p>
                                                         </div>
                                                     </div>
                                                     <div className="d-flex flex-row align-items-center">
@@ -111,7 +111,7 @@ function Promos() {
                                                         </div>
                                                         <div style={{ width: "80px" }}>
                                                             <MDBTypography tag="h5" className="mb-0">
-                                                                R14 500
+                                                                R800
                                                             </MDBTypography>
                                                         </div>
                                                         <a href="#!" style={{ color: "#cecece" }}>
@@ -222,7 +222,7 @@ function Promos() {
 //                 <li>
 //                     <a class="dropdown-item" href="https://media.istockphoto.com/id/638162758/photo/girlfriends-going-shopping.jpg?s=612x612&w=0&k=20&c=8CSDJdAN_QMaAuD3ejiys9faRW0GdfG-ra9l4nbD5RM=">PROMOTIONS for Womens</a></li>
 //                 <li><a class="dropdown-item" href="https://ae01.alicdn.com/kf/Sf195866fb29340778bc6b13b51c952a6i/Tracksuits-Women-Casual-Solid-Warm-Suits-Hoodies-Sweatpants-Autumn-Winter-Pullover-Sweatshirts-Pants-Sports-Suit-Two.jpg_220x220q90.jpg_.webp">2 Ladies Sweaters and Joggers for R500</a></li>
-//                 <li><a class="dropdown-item" href="https://media.istockphoto.com/id/1085149318/photo/sale-sign-at-the-entrance-of-clothing-store.webp?b=1&s=170667a&w=0&k=20&c=ulyPJOrWKbpuOZjSjmROQjDBpTHGzAvt1C5m3dyL_rA=">2 ladies Knitwear for R 800</a></li>
+//                 
 //                 <li><a class="dropdown-item" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTijmnqzNy-t2Zrrr6esfUo9UAZKpy9S2np5w&usqp=CAU">2 Ladies Bodycon Skirts for R650</a></li>
 //                 <li><a class="dropdown-item" href="https://images.unsplash.com/photo-1542272604-787c3835535d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8MiUyMExhZGllcyUyMEplYW5zJTIwZm9yJTIwUjEwMDB8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"> 2 Ladies Jeans for R1000</a></li>
 //                 <li><a class="dropdown-item" href="https://images.unsplash.com/photo-1621665422246-fde75fb7e7f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8N3xnOXpqODVsRExIRXx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60">2 ladies Shoes Fashions for R1 800</a></li>

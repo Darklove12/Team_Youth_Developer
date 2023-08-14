e4b939616ed34a00db1fe27952959da3d5cdaa24
Darklove12 <bsinukela31@gmail.com>
2023-08-14 14:55:52 +0200

    remove mdb

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,6 @@ import {
 import router from './Router'
 import ContextState from './Context/ContextState.jsx'
 
-// fortunate khumalo import from MDB install for Promo Project //
-
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

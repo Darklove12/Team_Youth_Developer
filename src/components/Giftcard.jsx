80e00865f41336c8b7527d58cda8b2f4e06734e7
Tshepo2022 <tshepotebe@gmail.com>
2023-08-16 10:56:57 +0200

    import added

--- src/components/Giftcard.jsx
+++ src/components/Giftcard.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
-// import './Giftcard.css'
-// function Giftcard() {
-//   return (
-//     <div>
-//       <div class='container-giftcard'>
-//       <article class="gift-card">
-//         <div class="gift-card__image">
-//         </div>
-//         <section class="gift-card__content">
-//           <div class="gift-card__amount">Reward</div>
-//           <div class="gift-card__amount-remaining">R50 Remaining</div>
-//           <div class="gift-card__code">645D 4353 FF77 DFG8</div>
-//           <div class="gift-card__msg">Use this code at checkout to redeem your gift card</div>
-//         </section>
-//       </article>
-//       </div>
+import './Giftcard.css'
+function Giftcard() {
+  return (
+    <div>
+      {/* <div class='container-giftcard'>
+      <article class="gift-card">
+        <div class="gift-card__image">
+        </div>
+        <section class="gift-card__content">
+          <div class="gift-card__amount">Reward</div>
+          <div class="gift-card__amount-remaining">R50 Remaining</div>
+          <div class="gift-card__code">645D 4353 FF77 DFG8</div>
+          <div class="gift-card__msg">Use this code at checkout to redeem your gift card</div>
+        </section>
+      </article>
+      </div> */}
 
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default Giftcard
+export default Giftcard

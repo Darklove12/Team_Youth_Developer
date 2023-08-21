04b726f76ee2afda067105e371cab13cbec0165f
unknown <you@example.com>
2023-08-21 11:03:57 +0200

    copy new add from card

--- src/components/Promos.jsx
+++ src/components/Promos.jsx
@@ -3,7 +3,98 @@ import './Promos.css';
 
 function Promos() {
   return (
-    <div>Promos</div>
+    <div> 
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="card-wrapper container-sm d-flex  justify-content-around">
+      <div class="card  " style="width: 18rem;">
+  <img src="https://source.unsplash.com/collection/190727/1600x900" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    
+  </div>
+</div><div class="card" style="width: 18rem;">
+  <img src="https://source.unsplash.com/collection/190727/1600x900" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    
+  </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+  <img src="https://source.unsplash.com/collection/190727/1600x900" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    
+  </div>
+</div>
+</div>
+    </div>
+    <div class="carousel-item">
+      <div class="card-wrapper container-sm d-flex   justify-content-around">
+      <div class="card  " style="width: 18rem;">
+  <img src="https://source.unsplash.com/collection/190727/1600x900" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    
+  </div>
+</div><div class="card" style="width: 18rem;">
+  <img src="https://source.unsplash.com/collection/190727/1600x900" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    
+  </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+  <img src="https://source.unsplash.com/collection/190727/1600x900" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    
+  </div>
+</div>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card-wrapper container-sm d-flex  justify-content-around">
+      <div class="card " style="width: 18rem;">
+  <img src="https://source.unsplash.com/collection/190727/1600x900" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    
+  </div>
+</div>
+        <div class="card" style="width: 18rem;">
+  <img src="https://source.unsplash.com/collection/190727/1600x900" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    
+  </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+  <img src="https://source.unsplash.com/collection/190727/1600x900" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    
+  </div>
+</div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    
+    
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  
+
+</div>
   )
 }
 
